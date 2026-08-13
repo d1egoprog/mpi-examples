8bd4b408182c63bc0060f69bd0d1474275ea526b
Diego Rincon-Yanez <d1egoprog@users.noreply.github.com>
2026-08-13 17:40:54 +0200

    Refactor main and procesandoAlgo functions
    Refactor printing and sleep logic in main and procesandoAlgo functions.

--- test3.c
+++ test3.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void procesandoAlgo(int numero);
+
 int main(int argc, char **argv) {
   int proceso; 
   int tamano;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &proceso); 
   MPI_Comm_size(MPI_COMM_WORLD, &tamano);
-  printf("Hola Mundo, me ejecuto en el procesador %d de un total de %d\n", proceso, tamano);
-  if (proceso == 0)
-	printf("Se acabo\n");
+  procesandoAlgo(proceso);
   MPI_Finalize();
 }
 
@@ -19,6 +19,6 @@ void procesandoAlgo(int numero){
 	for (int i=0;i<numero;i++)
 		printf("Imprimiendo %i de %i\n",i, numero);
 	printf("Termine de imprimir, ahora me toca esperar %i segundos\n", numero);
-	sleep(numero*1000);
+	sleep(numero);
 	return;
 }
